9a78b3a9a6960095776d52d0c4fe17f858b59cb0
minervatbd <minervatbd@gmail.com>
2025-02-23 15:55:12 -0500

    add a login page

---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,13 @@
+import PageTitle from '../components/PageTitle.tsx';
+import Login from '../components/Login.tsx';
+
+const LoginPage = () =>
+{
+    return(
+    <div>
+        <PageTitle />
+        <Login />
+    </div>
+    );
+};
+export default LoginPage;
