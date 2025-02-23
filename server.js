d45dd7153a3e537f082e34a527fd3530fa1d0b70
minervatbd <minervatbd@gmail.com>
2025-02-23 17:17:34 -0500

    tweak bodyparser call

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 app.use(cors());
-app.use(bodyParser.tsxon());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) =>
 {
 res.setHeader('Access-Control-Allow-Origin', '*');
