8b207b3a185597c472250f3c42f1d86643318586
minervatbd <minervatbd@gmail.com>
2025-02-23 19:06:38 -0500

    change switch to routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
 //import React from 'react';
-import { BrowserRouter as Router, Route, Navigate, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Navigate, Routes } from 'react-router-dom';
 import './App.css';
 import LoginPage from './pages/LoginPage';
 import CardPage from './pages/CardPage';
 function App() {
 return (
 <Router >
-<Switch>
+<Routes>
 <Route path="/" exact>
 <LoginPage />
 </Route>
@@ -14,7 +14,7 @@ return (
 <CardPage />
 </Route>
 <Navigate to="/" />
-</Switch>
+</Routes>
 </Router>
 );
 }
